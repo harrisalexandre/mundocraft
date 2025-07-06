1980660d2374d7d85f0817ba3ad2363150e38885
Harris Alexandre <h.lopes.ext@e-ai.com.br>
2025-07-05 23:04:26 -0300

    update routes in config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 const BASE_PATH = import.meta.env.BASE_URL;
 
 export const IMAGE_PATH =
-  import.meta.env.MODE === "development" ? "/images/" : "/mundocraft/images/";
+  import.meta.env.MODE === "development" ? "/images/" : "/images/";
